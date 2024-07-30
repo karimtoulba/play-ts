4a31df0ef75cd812e13b0401862ee9ba1958e199
Karim Toulba <karim@wadipurple.com>
2024-07-31 01:07:04 +0300

    Update play-1.ts

--- play-1.ts
+++ play-1.ts
@@ -8,5 +8,6 @@ Date: 30/07/2024
 tsc // creates an identical javascript file for debugging
 node index.js // run the created file to check if any problems happen
 
-// It's a string variable
+// It's a string/boolean variable
 let myVar : string;
+let myVar : boolean;
